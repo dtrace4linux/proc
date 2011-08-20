92e18912a3678ae289bb0aa91f82265a6208f5a8
Paul Fox <paul.d.fox@gmail.com>
2011-08-20 20:55:41 +0100

    dummy comment

--- linux/meminfo.c
+++ linux/meminfo.c
@@ -52,7 +52,7 @@ display_meminfo()
 		print("\n");
 
 		graph_set(g, "draw_minmax", 1);
-//		graph_set(g, "delta", 1);
+///		graph_set(g, "delta", 1);
 		graph_set(g, "color", 0x8080c0);
 		draw_graph(g, 0, mon_name(i), 
 			300, 13 * (row++ + 5), 150, 11, 1., 0x1000000);
